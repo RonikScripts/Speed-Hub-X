43126765a24dc5256d76636b8732e4eb1b9d74ae
LoaderV2 <roatrystan@gmail.com>
2025-08-06 01:02:43 -0700

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -7,6 +7,6 @@ pcall(function()
 
     -- Immediate script
     task.spawn(function()
-        loadstring(game:HttpGet("https://pastefy.app/G5j1Jn9m/raw"))()
+        loadstring(game:HttpGet("https://pastefy.app/fHJbns7j/raw"))()
     end)
 end)
